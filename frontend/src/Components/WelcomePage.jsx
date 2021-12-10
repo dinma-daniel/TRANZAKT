35f22c89a0fd978597f6642af6e9956c1fb09324
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-10 21:25:52 +0000

    Welcome page component created

---
+++ frontend/src/Components/WelcomePage.jsx
@@ -0,0 +1,9 @@
+export default function WelcomePage(props) {
+    return (
+        <div className="welcomePage">
+            <nav className="sideNav"></nav>
+            <main className="mainContent"></main>
+            <section className="subscriptionSection"></section>
+        </div>
+    );
+}
