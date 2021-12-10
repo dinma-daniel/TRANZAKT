d52b5e14c8ac13d9e63f6c717843fedb75df8817
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-10 17:35:23 +0000

    Added the signUpPage component

---
+++ frontend/src/Components/SignUpPage.jsx
@@ -0,0 +1,10 @@
+import FormImage from "./formImage";
+import SignUp from "./SignUp";
+export default function SignUpPage(props) {
+  return (
+    <div className="signUpPage">
+      <FormImage />
+      <SignUp />
+    </div>
+  );
+}
