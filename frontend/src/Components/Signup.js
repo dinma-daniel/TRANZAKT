0d916dc24636014bd2d2ca597eabdfd172ef26e5
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-11 14:24:09 +0000

    Removed the data handling in the signUp form

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -1,14 +1,8 @@
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
+import axios from 'axios';
 
 export default function SignUp(props) {
-  const [user, setUser] = useState({
-    firstName : '', 
-    lastName: '', 
-    email: '', 
-    password: '', 
-    confirmPassword: ''
-  })
     return (
       <div className="signUpContainer">
         <header className="signUpHeader">Sign up on BRAND</header>
@@ -19,10 +13,7 @@ export default function SignUp(props) {
         <form 
           className="signUpForm"
           onSubmit={
-            e => {
-              e.preventDefault();
-              console.log(user);
-            }
+            e => e.preventDefault()
           }
           >
           <section className="usersNames">
@@ -30,97 +21,27 @@ export default function SignUp(props) {
               type="text"
               placeholder="First Name"
               className="name formInput"
-              value={user.firstName}
-              onChange={
-                e => {
-                  const input = e.currentTarget.value;
-                  const {lastName, email, password, confirmPassword} = user;
-                  setUser({
-                      firstName: input, 
-                      lastName, 
-                      email, 
-                      password, 
-                      confirmPassword
-                  })
-                }
-              }
             />
             <input
               type="text"
               placeholder="Last Name"
               className="name formInput"
-              value={user.lastName}
-              onChange={
-                e => {
-                  const input = e.currentTarget.value;
-                  const {firstName, email, password, confirmPassword} = user;
-                  setUser({
-                      firstName, 
-                      lastName: input, 
-                      email, 
-                      password, 
-                      confirmPassword
-                  })
-                }
-              }
             />
           </section>
           <input 
             type="text" 
             placeholder="Email" 
             className="formInput fullSize" 
-            value={user.email}
-            onChange={
-              e => {
-                const input = e.currentTarget.value;
-                const {lastName, firstName, password, confirmPassword} = user;
-                setUser({
-                    firstName, 
-                    lastName, 
-                    email: input, 
-                    password, 
-                    confirmPassword
-                })
-              }
-            }
           />
           <input
             type="password"
             placeholder="Password"
             className="formInput fullSize"
-            value={user.password}
-            onChange={
-              e => {
-                const input = e.currentTarget.value;
-                const {lastName, firstName, email, confirmPassword} = user;
-                setUser({
-                    firstName, 
-                    lastName, 
-                    email, 
-                    password: input, 
-                    confirmPassword
-                })
-              }
-            }
           />
           <input
             type="password"
             placeholder="Confirm Password"
             className="formInput fullSize"
-            value={user.confirmPassword}
-            onChange={
-              e => {
-                const input = e.currentTarget.value;
-                const {lastName, firstName, email, password} = user;
-                setUser({
-                    firstName, 
-                    lastName, 
-                    email, 
-                    password, 
-                    confirmPassword: input
-                })
-              }
-            }
           />
           <button className="formButton">Enter</button>
         </form>
