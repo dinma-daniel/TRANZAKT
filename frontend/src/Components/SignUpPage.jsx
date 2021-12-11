82fbc5f90c6d17a827d024832caa5981d54f111f
unknown <ddunebari@gmail.com>
2021-12-11 01:28:14 +0100

    tweaked UI

--- frontend/src/Components/SignUpPage.jsx
+++ frontend/src/Components/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import FormImage from "./FormImage";
-import SignUp from "./SignUp";
+import SignUp from "./Signup";
 export default function SignUpPage(props) {
   return (
     <div className="signUpPage">
--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function SignUp(props) {
     return (
       <div className="signUpContainer">
         <header className="signUpHeader">Sign up on BRAND</header>
         <p className="signInSection">
           <span>Have an account? </span>
+          <Link to="/">
           <span className="signInBold">Sign in</span>
+          </Link>
         </p>
         <form className="signUpForm">
           <section className="usersNames">
