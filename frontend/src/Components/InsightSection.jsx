46db0cd6a34ece83d69e07496e35676573aa29c3
Marvellous <ibironkemarv@gmail.com>
2021-12-12 23:04:05 +0100

    quick fix for kog out

--- frontend/src/Components/InsightSection.jsx
+++ frontend/src/Components/InsightSection.jsx
@@ -1,11 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 export default function InsightSection(props) {
+    const handleLogOut = async e => {
+        try {
+            e.preventDefault();
+            // send the username and password to the server
+            const { data } = await axios.post(`logout`);
+            localStorage.clear()
+            window.location.href = "/login"
+        }
+        catch (e) {
+            // this should appear as a dropdown indicating the error 
+            console.log(e.response.data.message)
+            console.log(e)
+        }
+
+    };
     return (
         <div className="insightColumn pageColumn">
             <header className="insightSectionHeader">
                 <h1 >Personal Insight</h1>
                 <section className="iconSection">
                     <img className="iconSectionIcon" src={require('../images/Notification.png').default} />
-                    <button> <img onClick={() => props.history.replace('/welcome')} className="iconSectionIcon" src={require('../images/Sign_out_circle_light.png').default} /></button>
+                    <button> <img onClick={handleLogOut} className="iconSectionIcon" src={require('../images/Sign_out_circle_light.png').default} /></button>
                 </section>
             </header>
             <section className="mainInsightSection">
