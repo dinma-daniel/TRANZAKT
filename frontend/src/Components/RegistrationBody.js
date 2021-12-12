2ae5a96b2c302c02cbea618b760747ce9aa8b870
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-12 13:38:42 +0000

    Handled conditional rendering of form section in registration body

--- frontend/src/Components/RegistrationBody.js
+++ frontend/src/Components/RegistrationBody.js
@@ -1,4 +1,11 @@
+import {useState} from 'react';
+
 const RegistrationBody = () => {
+    const [isTrialPayment, setIsTrialPayment] = useState(false);
+    const handleRadioButtonChange = (e) => {
+        const booleanValue = e.target.value == 'Yes' ? true : false;
+        setIsTrialPayment(booleanValue);
+    }
     return ( 
         <div className="registration">
             <form className="reg__form">
@@ -37,6 +44,8 @@ const RegistrationBody = () => {
                     type="radio"
                     value="Yes"
                     id="yes"
+                    name="is_trial_payment"
+                    onChange={handleRadioButtonChange}
                     />
                     <label for="yes">Yes</label>
                     
@@ -47,18 +56,22 @@ const RegistrationBody = () => {
                     type="radio"
                     value="No"
                     id="no"
+                    name="is_trial_payment"
+                    onChange={handleRadioButtonChange}
                     />
                     <label for="no">No</label>
                     </div>
                     </div>
                     <br/>
-                    <h3 className="">Would you like to continue at the end of the trial?</h3>
+                    {isTrialPayment && <>                
+                        <h3 className="">Would you like to continue at the end of the trial?</h3>
                     <div className="radios">
                         <div className="r">
                     <input
                     type="radio"
                     value="Yes"
                     id="yes"
+                    name="continue_trial"
                     />
                     <label for="yes">Yes</label>
                     </div>
@@ -68,6 +81,7 @@ const RegistrationBody = () => {
                     type="radio"
                     value="No"
                     id="no"
+                    name="continue_trial"
                     />
                     <label for="no">No</label>
                     </div>
@@ -81,6 +95,8 @@ const RegistrationBody = () => {
                     />
                     <h3 className="month">Months</h3>
                     </div>
+                    </>
+                    }
                     <br/>
                     <button className="formBtn">Enter</button>
             </form>
